ac78a62c6ffd7d1813f0ff99d77065e656c1357f
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2026-01-29 11:04:27 -0500

    gv

--- SSMSTemplates/Metadata/DateTimeQuery.sql
+++ SSMSTemplates/Metadata/DateTimeQuery.sql
@@ -4,21 +4,26 @@ SELECT
     [t].[schema_name]
   , [t].[object_name] AS [table_name]
   , [t].[type_desc]
-  , [t].[rows]
+  , [t].[rows] AS [SQL_ROW_COUNT]
+  , [r].[SF_ROW_COUNT]
   , [t].[create_date]
   , [t].[modify_date]
   , [c].[DateColumnsCnt]
+  , [c].[ColumnsCnt]
   , [c].[DateColumns]
+  , [c].[Columns]
 FROM [dbo].[SQLTables] [t]
 LEFT JOIN( SELECT
                [TABLE_SCHEMA]
              , [TABLE_NAME]
-             , COUNT(1) AS [DateColumnsCnt]
-             , STRING_AGG(CAST(CONCAT([COLUMN_NAME], ' ', [DATA_TYPE]) AS VARCHAR(MAX)), ', ') WITHIN GROUP(ORDER BY [COLUMN_NAME]) AS [DateColumns]
+             , SUM(CASE WHEN [DATA_TYPE] IN ('date', 'datetime', 'datetime2') THEN 1 ELSE 0 END) AS [DateColumnsCnt]
+             , COUNT(1) AS [ColumnsCnt]
+             , STRING_AGG(CASE WHEN [DATA_TYPE] IN ('date', 'datetime', 'datetime2') THEN CAST(CONCAT([COLUMN_NAME], ' ', [DATA_TYPE]) AS VARCHAR(MAX)) END, ', ') WITHIN GROUP(ORDER BY [COLUMN_NAME]) AS [DateColumns]
+             , STRING_AGG(CAST(CONCAT([COLUMN_NAME], ' ', [DATA_TYPE]) AS VARCHAR(MAX)), ', ') WITHIN GROUP(ORDER BY [COLUMN_NAME]) AS [Columns]
            FROM [dbo].[SQLColumns]
-           WHERE [DATA_TYPE] IN ('date', 'datetime', 'datetime2')
            GROUP BY [TABLE_SCHEMA]
                   , [TABLE_NAME] ) [c] ON [t].[schema_name] = [c].[TABLE_SCHEMA] AND [t].[object_name] = [c].[TABLE_NAME]
-WHERE [t].[rows] IS NOT NULL AND EXISTS ( SELECT * FROM [Comparison_2] [r] WHERE [r].[Missing] IS NULL AND [r].[sql_table_schema] = [t].[schema_name] AND [r].[table_name] = [t].[object_name] )
+LEFT JOIN [Comparison_2] [r] ON [r].[Missing] IS NULL AND [r].[sql_table_schema] = [t].[schema_name] AND [r].[TABLE_NAME] = [t].[object_name]
+WHERE [t].[rows] IS NOT NULL AND [r].[sql_table_schema] IS NOT NULL
 ORDER BY [t].[schema_name]
        , [t].[object_name] ;
