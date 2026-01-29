f66b1139c87ddea4b23ed0b0e59a71858441c397
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2026-01-29 11:40:43 -0500

    gv

--- SSMSTemplates/Metadata/DateTimeQuery.sql
+++ SSMSTemplates/Metadata/DateTimeQuery.sql
@@ -1,18 +1,61 @@
 USE [Velera] ;
 GO
+DROP TABLE IF EXISTS [#CommonColumns] ;
+
 SELECT
-    [t].[schema_name]
-  , [t].[object_name] AS [table_name]
+    [c].[TABLE_SCHEMA]
+  , [c].[TABLE_NAME]
+  , [c].[COLUMN_NAME]
+  , [c].[DATA_TYPE]
+  , MAX([c].[ORDINAL_POSITION]) AS [max_ordinal_position]
+INTO [#CommonColumns]
+FROM [dbo].[SnowflakeColumns] [s]
+INNER JOIN [dbo].[SQLColumns] [c] ON [s].[TABLE_NAME] = [c].[TABLE_NAME] AND [s].[COLUMN_NAME] = [c].[COLUMN_NAME]
+WHERE [s].[DATA_TYPE] IN ('TIMESTAMP_LTZ', 'DATE', 'TIMESTAMP_NTZ') AND [c].[DATA_TYPE] IN ('date', 'datetime', 'datetime2')
+GROUP BY [c].[TABLE_SCHEMA]
+       , [c].[TABLE_NAME]
+       , [c].[COLUMN_NAME]
+       , [c].[DATA_TYPE] ;
+
+DROP TABLE IF EXISTS [#ColumnMatches] ;
+
+SELECT
+    [t].[schema_name] AS [SQL_TABLE_SCHEMA]
+  , [r].[SF_TABLE_SCHEMA]
+  , [t].[object_name] AS [TABLE_NAME]
   , [t].[type_desc]
   , [t].[rows] AS [SQL_ROW_COUNT]
   , [r].[SF_ROW_COUNT]
   , [t].[create_date]
   , [t].[modify_date]
+  , [mc].[MatchedDateColumn]
+  , [mc].[CommonDateColumnCnt]
   , [c].[DateColumnsCnt]
   , [c].[AllColumnsCnt]
+  , [mc].[CommonDateColumns]
   , [c].[DateColumns]
   , [c].[AllColumns]
+INTO [#ColumnMatches]
 FROM [dbo].[SQLTables] [t]
+LEFT JOIN( SELECT
+               [c].[TABLE_SCHEMA]
+             , [c].[TABLE_NAME]
+             , [c].[CommonDateColumnCnt]
+             , [c].[CommonDateColumns]
+             , ( SELECT TOP 1
+                        [cc].[COLUMN_NAME]
+                 FROM [#CommonColumns] [cc]
+                 WHERE [cc].[TABLE_SCHEMA] = [c].[TABLE_SCHEMA] AND [cc].[TABLE_NAME] = [c].[TABLE_NAME]
+                 ORDER BY CASE WHEN [cc].[COLUMN_NAME] LIKE 'load%date%' THEN 1 WHEN [cc].[COLUMN_NAME] LIKE 'modified%date' THEN 2 WHEN [cc].[COLUMN_NAME] LIKE 'create%date%' THEN 3 ELSE 4 END
+                        , [cc].[max_ordinal_position] DESC ) AS [MatchedDateColumn]
+           FROM( SELECT
+                     [c].[TABLE_SCHEMA]
+                   , [c].[TABLE_NAME]
+                   , COUNT(1) AS [CommonDateColumnCnt]
+                   , STRING_AGG(CAST(CONCAT([c].[COLUMN_NAME], ' ', [c].[DATA_TYPE]) AS VARCHAR(MAX)), ', ') WITHIN GROUP(ORDER BY [c].[COLUMN_NAME]) AS [CommonDateColumns]
+                 FROM [#CommonColumns] [c]
+                 GROUP BY [c].[TABLE_SCHEMA]
+                        , [c].[TABLE_NAME] ) [c] ) [mc] ON [mc].[TABLE_SCHEMA] = [t].[schema_name] AND [mc].[TABLE_NAME] = [t].[object_name]
 LEFT JOIN( SELECT
                [TABLE_SCHEMA]
              , [TABLE_NAME]
@@ -23,7 +66,52 @@ LEFT JOIN( SELECT
            FROM [dbo].[SQLColumns]
            GROUP BY [TABLE_SCHEMA]
                   , [TABLE_NAME] ) [c] ON [t].[schema_name] = [c].[TABLE_SCHEMA] AND [t].[object_name] = [c].[TABLE_NAME]
-LEFT JOIN [Comparison_2] [r] ON [r].[Missing] IS NULL AND [r].[sql_table_schema] = [t].[schema_name] AND [r].[TABLE_NAME] = [t].[object_name]
-WHERE [t].[rows] IS NOT NULL AND [r].[sql_table_schema] IS NOT NULL
+LEFT JOIN [dbo].[Comparison_2] [r] ON [r].[Missing] IS NULL AND [r].[SQL_TABLE_SCHEMA] = [t].[schema_name] AND [r].[TABLE_NAME] = [t].[object_name]
+WHERE [t].[rows] IS NOT NULL AND [r].[SQL_TABLE_SCHEMA] IS NOT NULL
 ORDER BY [t].[schema_name]
        , [t].[object_name] ;
+
+-- SQL POOL
+SELECT 'CREATE TABLE #TEMP(TABLE_SCHEMA VARCHAR(128), TABLE_NAME VARCHAR(128), COLUMN_NAME VARCHAR(128), COLUMN_VALUE DATETIME2(3), ROW_COUNT BIGINT);
+
+' AS SQL
+UNION ALL
+SELECT CONCAT('INSERT INTO #TEMP
+SELECT ''', [SQL_TABLE_SCHEMA], ''' AS TABLE_SCHEMA, ''', [TABLE_NAME], ''' AS [TABLE_NAME], ''', [MatchedDateColumn], ''' AS COLUMN_NAME, ', [MatchedDateColumn], ' AS COLUMN_VALUE, COUNT_BIG(1) AS ROW_COUNT
+FROM ', [SQL_TABLE_SCHEMA], '.', [TABLE_NAME], '
+GROUP BY ', [MatchedDateColumn], ';
+
+') AS SQL
+FROM(SELECT TOP 999999 *
+FROM [#ColumnMatches]
+WHERE [MatchedDateColumn] IS NOT NULL 
+ORDER BY [SQL_TABLE_SCHEMA],[TABLE_NAME]) A
+UNION ALL
+SELECT '
+SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, COLUMN_VALUE, ROW_COUNT
+FROM #TEMP
+ORDER BY TABLE_SCHEMA, TABLE_NAME;' AS SQL;
+
+
+
+
+SELECT 'CREATE OR REPLACE TEMPORARY TABLE TEMP(TABLE_SCHEMA VARCHAR(128), TABLE_NAME VARCHAR(128), COLUMN_NAME VARCHAR(128), COLUMN_VALUE TIMESTAMP_NTZ, ROW_COUNT BIGINT);
+
+' AS SQL
+UNION ALL
+SELECT CONCAT('INSERT INTO TEMP
+SELECT ''', [SF_TABLE_SCHEMA], ''' AS TABLE_SCHEMA, ''', [TABLE_NAME], ''' AS [TABLE_NAME], ''', [MatchedDateColumn], ''' AS COLUMN_NAME, ', [MatchedDateColumn], ' AS COLUMN_VALUE, COUNT(1) AS ROW_COUNT
+FROM ', [SF_TABLE_SCHEMA], '.', [TABLE_NAME], '
+GROUP BY ', [MatchedDateColumn], ';
+
+') AS SQL
+FROM(SELECT TOP 999999 *
+FROM [#ColumnMatches]
+WHERE [MatchedDateColumn] IS NOT NULL 
+ORDER BY [SQL_TABLE_SCHEMA],[TABLE_NAME]) A
+UNION ALL
+SELECT '
+SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, COLUMN_VALUE, ROW_COUNT
+FROM TEMP
+ORDER BY TABLE_SCHEMA, TABLE_NAME;' AS SQL;
+
